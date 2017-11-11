8ac1f12d223637745c320491ea023cdcb4dad829
Anton Fifindik <fifindik.av@gmail.com>
2017-11-11 18:58:25 +0200

    added several methods into interface dao

--- src/main/java/com/optimalroute/app/dao/MySqlOrderDao.java
+++ src/main/java/com/optimalroute/app/dao/MySqlOrderDao.java
@@ -1,5 +1,7 @@
 package com.optimalroute.app.dao;
 
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.optimalroute.app.interfaces.OrderDao;
+import com.optimalroute.app.objects.Order;
 
 @Component("mySqlOrderDao")
 public class MySqlOrderDao implements OrderDao {
@@ -16,4 +19,35 @@ public class MySqlOrderDao implements OrderDao {
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
+
+	@Override
+	public void insert(Order order) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insert(List<Order> list) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Order order) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Order> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/com/optimalroute/app/interfaces/OrderDao.java
+++ src/main/java/com/optimalroute/app/interfaces/OrderDao.java
@@ -1,5 +1,17 @@
 package com.optimalroute.app.interfaces;
 
-public interface OrderDao {
+import java.util.List;
 
+import com.optimalroute.app.objects.Order;
+
+public interface OrderDao {
+	void insert(Order order);
+
+	void insert(List<Order> list);
+
+	void delete(Order order);
+
+	void delete(int id);
+
+	List<Order> findAll();
 }
